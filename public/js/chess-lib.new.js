48637c46008bd1b94d871e00056d4fa50c9c7360
marcellino-ornelas <marcellino.ornelas@gmail.com>
2018-02-07 10:58:13 -0800

    (saving progress) working on logic for possible moves

--- public/js/chess-lib.new.js
+++ public/js/chess-lib.new.js
@@ -3,7 +3,7 @@
 const env = {
   turn: "white",
   history:[],
-  isGameOver: false;
+  isGameOver: false,
 };
 
 $.fn.extend({
@@ -75,7 +75,11 @@ board.calcNewSquare = function(oldSquare,addToRow,addToCol){
 
 const boardSpaces = 'a1 a2 a3 a4 a5 a6 a7 a8 b1 b2 b3 b4 b5 b6 b7 b8 c1 c2 c3 c4 c5 c6 c7 c8 d1 d2 d3 d4 d5 d6 d7 d8 e1 e2 e3 e4 e5 e6 e7 e8 f1 f2 f3 f4 f5 f6 f7 f8 g1 g2 g3 g4 g5 g6 g7 g8 h1 h2 h3 h4 h5 h6 h7 h8'.split(' ');
 
-// all chess pieces information
+/*
+ * chess Pieces
+ * This object will hold information about each piece
+ * and special methods that will update the Chess Piece object
+*/
 const chessPieces = {};
 
 chessPieces.allGamePieces = ['pawn', 'queen', 'king', 'castle','bishop','horse'];
@@ -83,29 +87,35 @@ chessPieces.allGamePieces = ['pawn', 'queen', 'king', 'castle','bishop','horse']
 
 _.each( chessPieces.allGamePieces, function(pieceName, index, orgArray){
   chessPieces[pieceName] = {};
-  chessPieces[pieceName].initializeUnique = _.noop;
+  chessPieces[pieceName].initialize2Unique = _.noop;
 });
 
 
-chessPieces.pawn.possibleMoves = [[0,1],[0,2]];
-chessPieces.king.possibleMoves = [[0,-1],[-1,-1],[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1]];
+chessPieces.pawn.possibleMoves   = [[0,1],[0,2]];
+chessPieces.king.possibleMoves   = [[0,-1],[-1,-1],[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1]];
 chessPieces.castle.possibleMoves = [[0,Infinity],[0, -Infinity],[Infinity,0],[-Infinity,0]];
-chessPieces.horse.possibleMoves = [[-1,2],[-1,-2],[1,2],[1,-2],[-2,-1],[-2,1],[2,-1],[2,1]];
+chessPieces.horse.possibleMoves  = [[-1,2],[-1,-2],[1,2],[1,-2],[-2,-1],[-2,1],[2,-1],[2,1]];
 chessPieces.bishop.possibleMoves = [[Infinity,Infinity],[-Infinity, Infinity],[-Infinity, -Infinity],[Infinity, -Infinity]];
-chessPieces.queen.possibleMoves = chessPieces.bishop.possibleMoves.concat(chessPieces.castle.possibleMoves);
+chessPieces.queen.possibleMoves  = chessPieces.bishop.possibleMoves.concat(chessPieces.castle.possibleMoves);
 
 chessPieces.pawn.initializeUnique = function(){};
 chessPieces.castle.initializeUnique = function(){};
 chessPieces.king.initializeUnique = function(){};
 
 
+chessPieces.pawn.canMove    = _.noop;
+chessPieces.king.canMove    = _.noop;
+chessPieces.castle.canMove  = _.noop;
+chessPieces.horse.canMove   = _.noop;
+chessPieces.bishop.canMove  = _.noop;
+chessPieces.queen.canMove   = _.noop;
+
 
 //Private function
 
-
-
 /*
-* constructor for single chess Piece
+ * Chess Piece
+ * This constructor will make a new chess piece
 */
 
 function ChessPiece($piece){
@@ -128,20 +138,85 @@ ChessPiece.prototype.calcMoves = function(){
 
   pieceInfo.initializeUnique(moves);
 
-  for(let i = 0, tracker = 0; i < pieceInfo.possibleMoves.length; i++){
+  for(let i = 0; i < pieceInfo.possibleMoves.length; i++){
 
     let [row,col] = pieceInfo.possibleMoves[i];
 
-    if(( !isFinity(row) ) || ( !isFinity(col) )){
-      _.noop();
-    } else {
-      let move = this.calcNewSquare(currentSquare, row, col);
+    const isRowInfinity = !isFinity(row);
+    const isColInfinity = !isFinity(col);
+    const isRowNegative = row < 0 ? - : false;
+    const isColNegative = col < 0 ? true : false;
 
-      if(move) this.moves.push(move);
+    parseInfinity = function(num){
+      return (num)
+    }
+
+    // row = isRowInfinity ?
+    let newPosition = currentSquare;
+    let stopper = 1;
+    let tracker = 0;
+
+    if(isRowInfinity || isColInfinity ){
+      // keep loop going until there is no newPosition
+      stopper = Infinity;
+
+      row = isNegative
+      col = isNegative
 
     }
+
+
+
+
+    do{
+      // get new position
+      // check if piece can move there
+        // see if team mate is there
+        // see if enemy is there
+        // see if empty
+      // push to moves
+
+      newPosition = board.calcNewSquare(newPosition, row, col);
+
+      if( newPosition ){
+
+
+
+        tracker++
+
+      } else{ break; }
+
+
+
+    } while( tracker < stopper)
+
+    // if( isRowInfinity || isColInfinity ){
+
+
+    // } else {
+    //   let move = this.calcNewSquare(currentSquare, row, col);
+
+    //   if(move) this.moves.push(move);
+
+    // }
   }
 }
+
+
+
+// this constructor will make a object model of the chess board
+// function Board(DOM){}
+
+// // see if square is empty
+// Board.prototype.isOpen = function(squareId){};
+
+// Board.prototype.replicate = function(){};
+
+// Board.prototype.populate = function(){};
+
+/************************
+^^^^^^^ GAME LOGIC ^^^^^^
+*************************/
 
 
 
